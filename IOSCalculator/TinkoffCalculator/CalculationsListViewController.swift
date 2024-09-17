c840ea19bb91691440b757b1c2271697e17be0e3
Victoria Chelnokova <148325729+tori-da-vi@users.noreply.github.com>
2024-09-17 20:13:24 +0300

    Add files via upload

---
+++ IOSCalculator/TinkoffCalculator/CalculationsListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CalculationsListViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Victoria Chelnokova on 07.03.2024.
+//
+
+import Foundation
+import UIKit
+
+class CalculationsListViewController: UIViewController {
+    
+    var result: String?
+    @IBOutlet weak var calculationLabel: UILabel!
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        initialize()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initialize()
+    }
+    
+    private func initialize() {
+        modalPresentationStyle = .fullScreen
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculationLabel.text = result
+    }
+}
